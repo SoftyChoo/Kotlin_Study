c459b8b45741a517e75b0b77f9f45b22542bb110
추민수 <devsoftychoo@gmail.com>
2023-07-24 16:43:58 +0900

    DOCS

---
+++ app/src/main/java/com/example/kttest/main.kt
@@ -0,0 +1,10 @@
+package com.example.kttest
+
+fun main() {
+    var accessTestClass = AccessTestClass()
+    var accessTestChildClass = AccessTestChildClass()
+
+// . 하고 접근가능한 요소를 확인
+//    accessTestClass.
+    accessTestChildClass.protectedTest1()
+}
