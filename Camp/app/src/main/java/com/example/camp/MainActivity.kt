44d94a75029f5c206ef2ca9c28ecb3e12291d6aa
추민수 <devsoftychoo@gmail.com>
2023-08-08 20:10:08 +0900

    [Feat] : tablayout & viewPager

--- Camp/app/src/main/java/com/example/camp/MainActivity.kt
+++ Camp/app/src/main/java/com/example/camp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.camp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //------------------------------------------------------------//
+        //FragmentAdatper 객체인 pageAdapter를 ViewPager의 Adpater로 연결 후
+        //ViewPager와 Tab을 setupWithViewPager로 연결해준다.
+        val pagerAdapter = FragmentAdapter(supportFragmentManager)
+        val pager = findViewById<ViewPager>(R.id.viewPager)
+        pager.adapter = pagerAdapter
+        val tab = findViewById<TabLayout>(R.id.tab)
+        tab.setupWithViewPager(pager)
+        //------------------------------------------------------------//
 
     }
 }
