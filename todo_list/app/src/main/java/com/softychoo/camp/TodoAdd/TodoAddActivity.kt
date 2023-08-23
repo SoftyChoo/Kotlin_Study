e65dddaf978c2eedf74048607806db47ca79bc24
추민수 <devsoftychoo@gmail.com>
2023-08-23 21:07:55 +0900

    [REFACTORING] : viewbinding으로 변경

--- todo_list/app/src/main/java/com/softychoo/camp/TodoAdd/TodoAddActivity.kt
+++ todo_list/app/src/main/java/com/softychoo/camp/TodoAdd/TodoAddActivity.kt
@@ -9,6 +9,7 @@ import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.snackbar.Snackbar
 import com.softychoo.camp.R
+import com.softychoo.camp.databinding.TodoAddActivityBinding
 import com.softychoo.camp.main.MainActivity
 import com.softychoo.camp.todo.TodoFragment
 import com.softychoo.camp.todo.TodoListAdapter
@@ -17,30 +18,31 @@ import com.softychoo.camp.todo.TodoModel
 
 class TodoAddActivity : AppCompatActivity() {
 
+    private lateinit var binding : TodoAddActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.todo_add_activity)
 
-        val toolbar = findViewById<Toolbar>(R.id.tool_bar)
-        val addBtn = findViewById<Button>(R.id.btn_registration)
-        val etTitle = findViewById<EditText>(R.id.et_title)
-        val etContent = findViewById<EditText>(R.id.et_content)
+        binding = TodoAddActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setSupportActionBar(toolbar)
+
+
+        setSupportActionBar(binding.toolBar)
         val ab = supportActionBar!!
 
         ab.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)//toolbar의 왼쪽에 backArrow 아이콘버튼을 넣음
         ab.setDisplayHomeAsUpEnabled(true)//아이콘을 보이게 만들어줌
-ß
-        toolbar.setNavigationOnClickListener {//toolbar의 뒤로가기 아이콘 클릭 시 화면종료
+
+        binding.toolBar.setNavigationOnClickListener {//toolbar의 뒤로가기 아이콘 클릭 시 화면종료
             setResult(RESULT_CANCELED)
             finish()
         }
 
-        addBtn.setOnClickListener {
+        binding.btnRegistration.setOnClickListener {
             val intentToMain = Intent()
-            intentToMain.putExtra("input_title",etTitle.text.toString())
-            intentToMain.putExtra("input_content",etContent.text.toString())
+            intentToMain.putExtra("input_title",binding.etTitle.text.toString())
+            intentToMain.putExtra("input_content",binding.etContent.text.toString())
             setResult(RESULT_OK,intentToMain)
             finish()
         }
