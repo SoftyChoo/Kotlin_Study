f80d6ac5e3c21c515d03a7df4b7b8a14adc69411
추민수 <devsoftychoo@gmail.com>
2023-09-15 15:21:10 +0900

    [Feat] : Shared Preferences

--- Test_SharedPreferneces/app/src/main/java/com/example/test_sharedpreferneces/MainActivity.kt
+++ Test_SharedPreferneces/app/src/main/java/com/example/test_sharedpreferneces/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.test_sharedpreferneces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.test_sharedpreferneces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    companion object {
+        val PREFERENCES_FILE_NAME: String = "pref_file_name"
+        val PREFERENCES_KEY : String = "key_name"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnSave.setOnClickListener {
+                saveData()
+                Toast.makeText(this@MainActivity,"Data Saved.",Toast.LENGTH_SHORT).show()
+            }
+        }
+        loadData()
     }
+
+    private fun loadData() {
+        val pref = getSharedPreferences(PREFERENCES_FILE_NAME,0)
+        binding.etHello.setText(pref.getString(PREFERENCES_KEY,""))
+    }
+
+    private fun saveData() {
+        val pref = getSharedPreferences(PREFERENCES_FILE_NAME, 0)
+        val edit = pref.edit() // 수정모드
+        // 첫번째 인자 -> Key
+        // 두번째 인자 -> 넣어줄 값
+        edit.putString(PREFERENCES_KEY,binding.etHello.text.toString())
+        edit.apply()
+    }
+
+
 }
